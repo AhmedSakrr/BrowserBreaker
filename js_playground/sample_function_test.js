f6a24892eb41f78960739be590ef2ed18b7d2fc4
0xcc-Since2016 <0xccsince2016@gmail.com>
2019-01-31 20:42:45 +0800

    slight adjust

--- js_playground/sample_function_test.js
+++ js_playground/sample_function_test.js
@@ -1,4 +1,5 @@
 //in js, function will be executed automatically when page loading.
+//this might be misleading...
 function testme(){
     var x = 0;
     console.log("Welcome to js playground.\n");
